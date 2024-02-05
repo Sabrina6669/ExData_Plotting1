6cedb6d83c118b60eeda6d01ef47426e70e9d0ec
Sabrina6669 <55246160+Sabrina6669@users.noreply.github.com>
2024-02-05 16:58:04 +0800

    plot hist figure

--- plot1.R
+++ plot1.R
@@ -9,3 +9,10 @@ head(subdata)
 subdata %>% 
   mutate(Date=as.Date(Date,"%m/%d/%y"),
          Time=strptime(Time, "%H:%M:%S"))
+
+##plot 1
+hist(subdata$Global_active_power,  main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)", col = "Red")
+##save file
+png("plot1.png", width=480, height=480)
+dev.off()
