67c7987b745f5daedeb51e71d12a50f808493341
Sabrina6669 <55246160+Sabrina6669@users.noreply.github.com>
2024-02-05 17:00:26 +0800

    revise code to save plot

--- plot1.R
+++ plot1.R
@@ -14,5 +14,5 @@ subdata %>%
 hist(subdata$Global_active_power,  main = "Global Active Power", 
      xlab = "Global Active Power (kilowatts)", col = "Red")
 ##save file
-png("plot1.png", width=480, height=480)
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
 dev.off()
